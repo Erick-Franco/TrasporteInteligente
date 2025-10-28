66209ad35878efa005285ff346799bd1a6fe8f41
unknown <rrickquispe@gmail.com>
2025-10-28 11:06:55 -0500

    ruta

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// ============ RUTAS ============
+// ============ RUTAS ===========
 app.get('/', (req, res) => {
   res.json({
     success: true,
